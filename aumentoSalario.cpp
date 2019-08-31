28450b233b829deff6fb67959e162beb4afda14e
nvitiver <nathan.vitiver@gmail.com>
2019-08-31 19:27:05 -0300

    Update aumentoSalario.cpp

--- aumentoSalario.cpp
+++ aumentoSalario.cpp
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
 int main(){
+    
+    //Variável salário
     float salario;
+    
+    //Pede o valor do salário para o usuário
     printf("Digite o salario: \n");
+    
+    //Pega o valor do salário e guarda dentro da variável salario
     scanf("%f", &salario);
-
+    
+    //Faz a conta de 25% de aumento e guarda dentro da variável salario
     salario = salario * 1.25;
+    
+    //Exibe na tela o resultado final
     printf("Salario + aumento de 25 por cento = %.2f\n", salario);
 
 }
