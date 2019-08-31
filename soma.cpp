651bdb2672293285a807870857b83c4131d68cbe
nvitiver <nathan.vitiver@gmail.com>
2019-08-31 16:30:33 -0300

    Comentários no código
    Documentação do código para que outros programadores entendam.

---
+++ soma.cpp
@@ -0,0 +1,20 @@
+//Biblioteca padrão do C
+#include <stdio.h>
+
+//Função que inicia o programa
+int main(){
+    //Declaração das variáveis e seus respectivos valores
+    int n1 = 2, n2 = 4, n3 = 1, n4 = 5;
+    
+    //Exibe em tela cada variável com os valores
+    printf("N1 = %d, N2 = %d, N3 = %d e N4 = %d\n", n1,n2,n3,n4);
+    
+    //Cria uma variável soma para guardar o resultando final
+    int soma;
+    
+    //Guarda o resultado final
+    soma = n1 + n2 + n3 + n4;
+    
+    //Exibe em tela o resultado final
+    printf("Soma: %d", n1)
+}
